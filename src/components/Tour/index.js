2422e450cba9a6d46cf495c79b6afbe7fe3ee59b
Heltzel <heltzel.service@hotmail.com>
2020-12-06 22:47:02 +0100

    Distributing the data

--- src/components/Tour/index.js
+++ src/components/Tour/index.js
@@ -5,30 +5,25 @@ import { FaCaretSquareDown } from 'react-icons/fa'
 
 export default class index extends Component {
   render() {
+    const { city, name, img, info } = this.props.tour
     return (
       <article className="tour">
         <div className="image-container">
-          <img
-            src="https://images.pexels.com/photos/5683079/pexels-photo-5683079.jpeg?cs=srgb&dl=pexels-boris-krstic-5683079.jpg&fm=jpg"
-            alt="name"
-          />
+          <img src={img} alt={name} />
           <span className="close-btn">
             <FaWindowClose />
           </span>
         </div>
         <div className="tour-info">
-          <h3>city</h3>
-          <h4>name</h4>
+          <h3>{city}</h3>
+          <h4>{name}</h4>
           <h5>
-            info{' '}
+            info
             <span>
               <FaCaretSquareDown />
             </span>
           </h5>
-          <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nesciunt,
-            eligendi!
-          </p>
+          <p>{info}</p>
         </div>
       </article>
     )
--- src/components/TourList/index.js
+++ src/components/TourList/index.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react'
 import './TourList.scss'
 import Tour from '../Tour'
+import { tourData } from '../../tourData'
 
 export default class TourList extends Component {
+  constructor() {
+    super()
+    this.state = {
+      tours: tourData,
+    }
+  }
   render() {
+    const { tours } = this.state
     return (
       <section className="tour-list">
-        <Tour />
+        {tours.map((tour) => {
+          return <Tour tour={tour} key={tour.id} />
+        })}
       </section>
     )
   }
